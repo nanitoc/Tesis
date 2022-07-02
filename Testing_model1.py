3a923f3a0311dcd90a2590335c2e05ae4b211330
nanitoc <86374201+nanitoc@users.noreply.github.com>
2022-07-02 17:07:19 -0400

    Update

--- Testing_model1.py
+++ Testing_model1.py
@@ -2,7 +2,8 @@ import cv2
 import tensorflow as tf
 import keras
 
-CATEGORIES = ["Alert" , "No Alert" ]
+CATEGORIES = ["Alert" , "No Alert " ]   #Para Modelo 1
+#CATEGORIES = ["Alert" , "No Alert 1", "No Alert 2" ]
 
 def prepare(filepath):
     IMG_SIZE=300
@@ -10,8 +11,18 @@ def prepare(filepath):
     new_array = cv2.resize(img_array, (IMG_SIZE,IMG_SIZE))
     return new_array.reshape(-1,IMG_SIZE,IMG_SIZE,1)
 
-model = keras.models.load_model("Alert-noAlert-64x3-CNN.model")
+model = keras.models.load_model("Alert-noAlert-64x3-CNN-Update.model")
 
-prediction = model.predict([prepare('Train/IMG2.jpg')])
+prediction = model.predict([prepare(f'Train/TEST_NO_ALERT (6).jpg')])
 print(CATEGORIES[int(prediction[0][0])])
+
+""""
+for x in range(1,5):
+ prediction = model.predict([prepare(f'Train/TEST_ALERT  ({x}).jpg')])
+ print(CATEGORIES[int(prediction[0][0])])
+
+for x in range(1,6):
+ prediction = model.predict([prepare(f'Train/TEST_NO_ALERT ({x}).jpg')])
+ print(CATEGORIES[int(prediction[0][0])])
 #'Train/Alert_T1.jpg',
+"""
